0d851b8357125d43175cbde48019ea304ed920e6
Tamas Szabo <szabogtamas@gmail.com>
2020-10-26 21:42:19 +0100

    Fixed error with space in file name

--- utils/latex_shortcuts.py
+++ utils/latex_shortcuts.py
@@ -55,7 +55,9 @@ def figure_fullwidth(path, caption=""):
         + caption
         + """}
         \\includegraphics[width=\\textwidth]{"""
+        + '"'
         + path
+        + '"'
         + """}
     \\end{figure}
     \\end{center}
